a1b016da6efd14b219511b884909c47ca0205856
PT <trivedp18@gmail.com>
2025-04-07 14:29:35 -0400

    to edit script and add moma_mediums_cleaned.csv

--- clean_moma.py
+++ clean_moma.py
@@ -9,3 +9,11 @@ df['Clean_Year'] = df['Date'].astype(str).str.extract(r'(\d{4})')
 print("After cleaning:", df[['Date', 'Clean_Year']].head(10))
 
 df.to_csv('moma_artworks_cleaned.csv', index=False)
+
+print(df['Medium'].unique()[:20])
+
+df['Medium_Clean'] = df['Medium'].astype(str).str.lower()
+
+print(df['Medium_Clean'].value_counts().head(10))
+
+df.to_csv('moma_mediums_cleaned.csv', index=False)
